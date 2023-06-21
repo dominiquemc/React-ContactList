e830994090cc4c20b3b43527a44bd9ffb8f13a5d
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-06-21 16:18:27 -0500

    removed class

---
+++ src/components/SelectedContact.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+export default function SelectedContact({
+  selectedContactId,
+  setSelectedContactId,
+}) {
+  const [contact, setContact] = useState(null);
+  console.log(contact);
+  useEffect(() => {
+    async function myContacts() {
+      try {
+        const response = await fetch(
+          `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`
+        );
+        const result = await response.json();
+        setContact(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    myContacts();
+  }, [selectedContactId]);
+  return (
+    <>
+      {contact && (
+        <div className="selected-contact">
+          <p>{contact.name}</p>
+          <p>{contact.email}</p>
+          <p>{contact.address.city}</p>
+          <p>{contact.address.zipcode}</p>
+          <p>{contact.website}</p>
+          <button>Back</button>
+        </div>
+      )}
+    </>
+  );
+}
